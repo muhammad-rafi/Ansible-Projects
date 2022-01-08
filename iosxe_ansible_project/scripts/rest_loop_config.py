bbdf25384f5f35f9f4f28be13cb3e561f156e755
Muhammad Rafi <murafi@cisco.com>
2022-01-08 02:42:58 +0000

    tweak rest_loop script

--- iosxe_ansible_project/scripts/rest_loop_config.py
+++ iosxe_ansible_project/scripts/rest_loop_config.py
@@ -9,6 +9,7 @@ def main():
     Run the playbook defined inside the function,
     and backup the configuration of iosxe device.
     """
+    
     playbook = "/opt/ansible/Ansible-Projects/iosxe_ansible_project/playbooks/rest_loop_playbook.yml"
     inventory = "/opt/ansible/Ansible-Projects/iosxe_ansible_project/inventory.yml"
     
@@ -31,7 +32,7 @@ def main():
         #     print(event['event'])
         
         if runner.status == 'successful':
-            print("Loopback interface(s) have been configure successfully.")
+            print("Loopback interface(s) have been configure successfully.\n")
             # print(json.dumps(stats, indent=4))
         else: 
             print(runner.status)
